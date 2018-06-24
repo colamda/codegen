2479f2b369ccc8cc662c43fbb4b6f74187413304
Arne Bosien <info@arnebosien.de>
2018-06-24 17:27:47 +0200

    add snake & kebab case

--- src/app/utils/case.utils.ts
+++ src/app/utils/case.utils.ts
@@ -8,4 +8,12 @@ export class CaseUtils {
   public static camelCase(str: string): string {
     return _.camelCase(str);
   }
+
+  public static kebabCase(str: string): string {
+    return _.kebabCase(str);
+  }
+
+  public static snakeCase(str: string): string {
+    return _.snakeCase(str);
+  }
 }
